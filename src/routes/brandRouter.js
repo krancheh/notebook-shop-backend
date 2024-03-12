ac7aff4824830229366545a0e963a0acf4cc10a0
Kirill <krancheh@gmail.com>
2024-03-13 00:42:44 +0300

    add delete route

--- src/routes/brandRouter.js
+++ src/routes/brandRouter.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/brandController');
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 router.post('/add', checkRoleMiddleware('ADMIN'), controller.add);
+router.delete('/delete', checkRoleMiddleware('ADMIN'), controller.delete);
 router.get('/getAll', controller.getAll);
 router.get('/:id', controller.getById);
 
