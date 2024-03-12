83bbc1f49baff3a15509a6405c9df660fe8f2616
Kirill <krancheh@gmail.com>
2024-03-13 00:41:52 +0300

    add fName lName to User model

--- src/models/models.js
+++ src/models/models.js
@@ -3,8 +3,10 @@ const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    firstName: { type: DataTypes.STRING(35), allowNull: false },
+    lastName: { type: DataTypes.STRING(35), allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING, allowNull: false },
     role: { type: DataTypes.STRING, defaultValue: 'USER' }
 });
 
