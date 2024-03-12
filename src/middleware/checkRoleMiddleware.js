bd2c871f19a93e67213559ac9806308235525f08
Kirill <krancheh@gmail.com>
2024-03-13 00:41:10 +0300

    add auth cookie

--- src/middleware/checkRoleMiddleware.js
+++ src/middleware/checkRoleMiddleware.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
+const ApiError = require("../error/ApiError");
 module.exports = function (role) {
     return function (req, res, next) {
-        if (res.method === 'OPTIONS') {
+        if (req.method === 'OPTIONS') {
             next()
         }
         try {
-            const token = req.headers.authorization.split(' ')[1]
+            const token = req.cookies["auth"].split(' ')[1];
+
             if (!token) {
-                res.status.json({message: 'Пользователь не авторизован'})
+                next(ApiError.unathorized('Пользователь не авторизован'));
             }
+
             const decoded = jwt.verify(token, process.env.SECRET_KEY)
             if (decoded.role !== role) {
-                res.status(403).json({message: 'У вас нет доступа'})
-
-                // TODO ПЕРЕДЕЛАТЬ, ТК ОНО ВСЕ РАВНО ПРОПУСКАЕТ К СОЗДАНИЮ БРЕНДА
+                next(ApiError.forbidden('У вас недостаточно прав'));
             }
-            req.user = decoded
+            req.user = decoded;
             next()
         } catch (e) {
-            res.status(401).json({message: 'Пользователь не авторизован'})
+            next(ApiError.internal('Произошла ошибка'));
         }
     }
 }
