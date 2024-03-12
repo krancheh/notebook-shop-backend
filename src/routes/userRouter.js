ecf64c763d633fde5f5f4fa4be75796fb6cf5c08
Kirill <krancheh@gmail.com>
2024-03-13 00:43:14 +0300

    change signup route path

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = new Router();
 const controller = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/registration', controller.registration);
+router.post('/signup', controller.registration);
 router.post('/login', controller.login);
 router.get('/auth', authMiddleware, controller.checkAuth);
 router.get('/:id', controller.getById);
