a107987f16a5e1d6e3a48a93697d98213c9dceef
Kirill <krancheh@gmail.com>
2024-03-13 23:38:33 +0300

    change routes naming

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,10 @@ const brandRouter = require('./brandRouter');
 const itemRouter = require('./itemRouter');
 const basketRouter = require('./basketRouter');
 
-router.use('/user', userRouter);
-router.use('/type', typeRouter);
-router.use('/brand', brandRouter);
-router.use('/item', itemRouter);
+router.use('/users', userRouter);
+router.use('/types', typeRouter);
+router.use('/brands', brandRouter);
+router.use('/items', itemRouter);
 router.use('/basket', basketRouter);
 
 
